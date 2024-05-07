0f9336d7f136623b12d08364ab74c0fea061b7ee
Barney Hanlon <barney@shrikeh.net>
2024-05-07 12:12:41 +0100

    Change namespace

--- src/Id/CorrelationId.php
+++ src/Id/CorrelationId.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace RpHaven\Uid\Id;
 
-use RpHaven\App\Correlation\CorrelationId as AppCorrelationId;
+use RpHaven\App\Message\Correlation\CorrelationId as AppCorrelationId;
 use RpHaven\Uid\Uid;
 
 interface CorrelationId extends Uid, AppCorrelationId
