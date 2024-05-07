f2b1b34c3f18892bc9ef27bfae7a59265e919b44
Barney Hanlon <barney@shrikeh.net>
2024-05-07 11:13:28 +0100

    Add correlation ID

--- src/Id/CorrelationId.php
+++ src/Id/CorrelationId.php
@@ -4,9 +4,10 @@ declare(strict_types=1);
 
 namespace RpHaven\Uid\Id;
 
+use RpHaven\App\Correlation\CorrelationId as AppCorrelationId;
 use RpHaven\Uid\Uid;
 
-interface CorrelationId extends Uid
+interface CorrelationId extends Uid, AppCorrelationId
 {
 
 }
